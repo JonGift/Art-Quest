e270baff862ba43ded3f945a3cf4f68aa5739df7
Jon <8bityamist@gmail.com>
2022-01-09 18:08:49 -0800

    Added ink painter asset with a lil paint brush

---
+++ Assets/pencilController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pencilController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject tip;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
