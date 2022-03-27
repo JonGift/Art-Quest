a8db269f1df1db3b1ed1ffed8914cc2c76709e6a
g-henkle <ghenkle.art@gmail.com>
2022-03-27 04:05:30 -0700

    Beer and cigarettes before bed

---
+++ Assets/GoodProps_LampSet/HDRP_GoodProps_LampSet/Scripts/LightSwitchHDRP.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightSwitchHDRP : MonoBehaviour
+{
+    public GameObject lightObject;
+    private Renderer _rend;
+
+    public bool isLightOn = true;
+    private bool _wasLightOn = false;
+
+    // Awake is called when the script instance is being loaded
+    void Awake()
+    {
+        _rend = GetComponent<Renderer>();
+        SwitchLight();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //check the state of light
+        if (isLightOn != _wasLightOn)
+        {
+            SwitchLight();
+        }
+    }
+
+    void SwitchLight()
+    {
+        var tempMaterial = new Material(_rend.sharedMaterial);
+
+        //turn on or off the light
+        //and change the emission of the lamp
+        if (isLightOn)
+        {
+            lightObject.SetActive(true);
+
+            //HDRP Lit material emission
+            tempMaterial.SetFloat("_EmissiveExposureWeight", 0.2f);
+
+        }
+        else
+        {
+            lightObject.SetActive(false);
+
+            //HDRP Lit material emission
+            tempMaterial.SetFloat("_EmissiveExposureWeight", 1f);
+
+
+        }
+        _rend.sharedMaterial = tempMaterial;
+        _wasLightOn = isLightOn;
+    }
+}
---
+++ Assets/GoodProps_LampSet/README.txt
@@ -0,0 +1,31 @@
+GoodProps_LampSet
+Lamp set for your projects
+
+Thank you for purchasing this asset!
+
+ 
+How to use in a standard Unity project: 
+1. Import the assets corresponding to your render pipeline : 
+		- CommonMeshes_GoodProps_LampSet
+		- Standard_GoodProps_LampSet
+
+2. Go to Standard_GoodProps_LampSet/Prefabs
+3. Drag and drop the object you want on your scene
+
+
+How to use in an HDRP Unity project:
+1. Import the assets corresponding to your render pipeline : 
+		- CommonMeshes_GoodProps_LampSet
+		- HDRP_GoodProps_LampSet
+
+2. Go to HDRP_GoodProps_LampSet/Prefabs_HDRP
+3. Drag and drop the object you want on your scene
+
+
+How to use the switch light script:
+1. Select the lamp and go in the inspector, find the SwitchLight script*
+and check the check box "IsLightOn" if you want to switch it on
+2. Play the scene, it works!
+
+*The material of each lamp includes an emissive texture to give a realistic lights effect
+The SwitchLight script allows switching on/off both the emissive texture and the Point light in the lamp.
