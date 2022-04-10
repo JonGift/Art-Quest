074b717c076f697bd022271239fec714b7433006
Jon <8bityamist@gmail.com>
2022-04-09 19:48:30 -0700

    sound stuff

---
+++ Assets/BedroomVolumeOverlord.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BedroomVolumeOverlord : MonoBehaviour
+{
+    public GameObject player;
+    public AudioSource source1;
+    public AudioSource source2;
+
+    float source1XPos;
+    float source2XPos;
+
+    float source1BaseVolume;
+    float source2BaseVolume;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
